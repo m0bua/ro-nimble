c8aa135ec87c445038f95d08be9d7e88173c9615
scream <tkachenko.n@rozetka.com.ua>
2020-07-08 17:37:42 +0300

    load elastic data

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,34 +2,35 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\ElasticsearchService;
-use App\Services\GoodsService;
+use App\Models\Elastic\Promotions\GoodsModel as ElasticGoodsModel;
+use App\Models\GraphQL\GoodsModel as GraphGoodsModel;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    public function goods(GoodsService $goodsService, ElasticsearchService $elasticsearchService)
+    public function goods(GraphGoodsModel $graphGoodsModel, ElasticGoodsModel $elasticGoodsModel)
     {
         $id = 200775625;
         $id = 97653;
         $id = 198516121;
+        $id = 199807069;
         $id = 108521;
 
-        $good = $goodsService->getById($id);
+        $goods = $graphGoodsModel->getOneById($id);
 
-        dump(
-            $good
-        );
-        die;
+        $elasticGoodsModel->load(array_merge([
+            'promotion_id' => 222,
+            'constructor_id' => 333,
+            'gift_id' => 444,
+        ], $goods));
 
-        $response = $elasticsearchService->setGood($good);
+        $response = $elasticGoodsModel->save();
+//        dump(
+//            $response
+//        );
+//        die;
 
-        dump(
-            $response
-        );
-        die;
-
-        $search = $elasticsearchService->searchGood($id);
+        $search = $elasticGoodsModel->searchById($id);
 
         dump(
             $search
---
+++ app/ValueObjects/NonExistingMethod.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\ValueObjects;
+
+use App\Helpers\ConvertString;
+use ReflectionClass;
+use Exception;
+
+class NonExistingMethod
+{
+    public const GET = 'get';
+    public const SET = 'set';
+
+    /**
+     * @var object
+     */
+    private $object;
+
+    /**
+     * @var string
+     */
+    private $methodName;
+
+    /**
+     * @var string
+     */
+    private $methodPrefix;
+
+    /**
+     * @var string
+     */
+    private $propertyName;
+
+    /**
+     * Property constructor.
+     * @param object $object
+     * @param string $methodName
+     */
+    public function __construct(object $object, string $methodName)
+    {
+        $this->methodName = $methodName;
+        $this->prefixValidate();
+
+        $this->object = $object;
+        $this->propertyValidate();
+    }
+
+    /**
+     * Validate prefix of method
+     */
+    public function prefixValidate()
+    {
+        $reflection = new ReflectionClass($this);
+        $prefix = ConvertString::getPrefix($this->methodName, $reflection->getConstants());
+
+        if (!$prefix) {
+            throw new \Exception(sprintf('Incorrect method %s', $this->methodName));
+        }
+
+        $this->methodPrefix = $prefix;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPrefix()
+    {
+        return $this->methodPrefix;
+    }
+
+    /**
+     * Validate property of method
+     * @throws Exception
+     */
+    public function propertyValidate()
+    {
+        $property = ConvertString::stringWithoutPrefix($this->methodName, $this->methodPrefix);
+
+        if (!property_exists($this->object, $property)) {
+            throw new Exception(sprintf('Property %s does not exists in %s', $this->methodName, get_class($this->object)));
+        }
+
+        $this->propertyName = $property;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPropertyName()
+    {
+        return $this->propertyName;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPropertyValue()
+    {
+        $property = ConvertString::camelCaseToSnake($this->propertyName);
+
+        if (array_key_exists($property, $this->object->getData())) {
+            return $this->object->getData()[$property];
+        }
+
+        return null;
+    }
+}
