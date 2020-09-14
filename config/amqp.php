ab5cd5ef3c36c2d78193f33a37f6a30b48e6d448
Aleksandr Bondarenko <bondarenko_al@rozetka.com.ua>
2020-09-14 17:35:39 +0300

    fix config

--- config/amqp.php
+++ config/amqp.php
@@ -113,9 +113,9 @@ return [
             'timeout'               => 0,
             'persistent'            => true,
 
-            'qos'                   => false,
+            'qos'                   => true,
             'qos_prefetch_size'     => 0,
-            'qos_prefetch_count'    => 1,
+            'qos_prefetch_count'    => 1000,
             'qos_a_global'          => false,
             'processor_name'        => function($routingKey) {
                 $keywords = explode('.', ucwords($routingKey, '.'));
