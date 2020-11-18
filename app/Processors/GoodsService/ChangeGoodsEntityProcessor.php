0e8668d7fd817bb84bfee064e8c09dac8a4d2c9d
Aleksandr Bondarenko <bondarenko_al@rozetka.com.ua>
2020-11-18 12:09:19 +0200

    fix update

--- app/Processors/GoodsService/ChangeGoodsEntityProcessor.php
+++ app/Processors/GoodsService/ChangeGoodsEntityProcessor.php
@@ -11,29 +11,58 @@ class ChangeGoodsEntityProcessor implements ProcessorInterface
 {
     public function processMessage(MessageInterface $message): int
     {
-        $goodsData = (array)$message->getField('data');
+        $updateFields = [
+            'title',
+            'name',
+            'category_id',
+            'mpath',
+            'price',
+            'rank',
+            'sell_status',
+            'group_id',
+            'is_group_primary',
+            'status_inherited',
+            'order',
+            'series_id',
+            'state',
+            'producer_id',
+            'seller_id',
+        ];
 
-        DB::table('goods')
-            ->where(['id' => $goodsData['id']])
-            ->update([
-                'title' => $goodsData['title'],
-                'name' => $goodsData['name'],
-                'category_id' => $goodsData['category_id'],
-                'mpath' => $goodsData['mpath'],
-                'price' => $goodsData['price'],
-                'rank' => $goodsData['rank'],
-                'sell_status' => $goodsData['sell_status'],
-                'group_id' => $goodsData['group_id'],
-                'is_group_primary' => $goodsData['is_group_primary'],
-                'status_inherited' => $goodsData['status_inherited'],
-                'order' => $goodsData['order'],
-                'series_id' => $goodsData['series_id'],
-                'state' => $goodsData['state'],
-                'producer_id' => $goodsData['producer_id'],
-                'seller_id' => $goodsData['seller_id'],
-                'needs_index' => 1,
-                'updated_at' => date('Y-m-d H:i:s'),
-            ]);
+        $goodsData = (array)$message->getField('data');
+        $changed = (array)$message->getField('changed');
+
+        $intersect = array_intersect($updateFields, $changed);
+
+        if (!empty($intersect)) {
+            $updateData = [];
+            foreach ($intersect as $field) {
+                $updateData[$field] = $goodsData[$field];
+            }
+
+            DB::table('goods')
+                ->where(['id' => $goodsData['id']])
+                ->update($updateData);
+//                ->update([
+//                    'title' => $goodsData['title'],
+//                    'name' => $goodsData['name'],
+//                    'category_id' => $goodsData['category_id'],
+//                    'mpath' => $goodsData['mpath'],
+//                    'price' => $goodsData['price'],
+//                    'rank' => $goodsData['rank'],
+//                    'sell_status' => $goodsData['sell_status'],
+//                    'group_id' => $goodsData['group_id'],
+//                    'is_group_primary' => $goodsData['is_group_primary'],
+//                    'status_inherited' => $goodsData['status_inherited'],
+//                    'order' => $goodsData['order'],
+//                    'series_id' => $goodsData['series_id'],
+//                    'state' => $goodsData['state'],
+//                    'producer_id' => $goodsData['producer_id'],
+//                    'seller_id' => $goodsData['seller_id'],
+//                    'needs_index' => 1,
+//                    'updated_at' => date('Y-m-d H:i:s'),
+//                ]);
+        }
 
         return Codes::SUCCESS;
     }
