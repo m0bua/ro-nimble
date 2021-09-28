7001c9f3d2d01c4e0afeabaec06493a2779a9e33
Dariy Klementyev <ss.wiking@outlook.com>
2021-09-28 23:06:51 +0300

    Fixed issue with property type

--- app/Processors/GoodsService/CreateOptionValueProcessor.php
+++ app/Processors/GoodsService/CreateOptionValueProcessor.php
@@ -20,4 +20,18 @@ class CreateOptionValueProcessor extends AbstractProcessor
     {
         $this->model = $model;
     }
+
+    /**
+     * @inheritDoc
+     */
+    protected function prepareData(): array
+    {
+        $data = parent::prepareData();
+
+        if (isset($data['show_value_in_short_set'])) {
+            $data['show_value_in_short_set'] = $data['show_value_in_short_set'] === 'true' ? 1 : 0;
+        }
+
+        return $data;
+    }
 }
