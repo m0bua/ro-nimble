ad4e526f935bf848b533564b6226126578bcef96
Александр Бондаренко <bondarenko_al@rozetka.com.ua>
2021-12-23 17:01:13 +0200

    update coposer and fix config

--- config/graphql.php
+++ config/graphql.php
@@ -3,9 +3,9 @@
 return [
 
     'goods' => [
-        'endpoint_url' => env('GQL_GOODS_SERVICE_URL'),
+        'endpoint_url' => env('GQL_GOODS_SERVICE_URL', ''),
         'authorization_headers' => [
-            'Authorization' => 'Basic '. base64_encode(env('GQL_GOODS_SERVICE_LOGIN') . ':' . env('GQL_GOODS_SERVICE_PASSWORD'))
+            'Authorization' => 'Basic '. base64_encode(env('GQL_GOODS_SERVICE_LOGIN', '') . ':' . env('GQL_GOODS_SERVICE_PASSWORD', ''))
         ],
         'http_options' => [],
     ]
